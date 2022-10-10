745c5978473c14587321d9e7a133a6f083a1c498
smoothbrady <bradyobholz@gmail.com>
2022-10-10 17:06:01 -0500

    1st

--- mongoose-express-liquid-boilerplate-main/server.js
+++ mongoose-express-liquid-boilerplate-main/server.js
@@ -4,9 +4,9 @@
 require("dotenv").config() // make env variables available
 const express = require("express")
 const middleware = require('./utils/middleware')
-const ExampleRouter = require('./controllers/example')
-const UserRouter = require('./controllers/user')
-const User = require("./models/user")
+const FishRouter = require('./controllers/fishcontrollers')
+const UserRouter = require('./controllers/userControllers')
+const Path = require("path")
 // SEE MORE DEPENDENCIES IN ./utils/middleware.js
 // user and resource routes linked in ./utils/middleware.js
 
@@ -21,20 +21,32 @@ middleware(app)
 //    Routes      //
 ////////////////////
 
-app.use('/auth', UserRouter)
-app.use('/examples', ExampleRouter)
+/////////////////////////////////////////////
+// Register our Routes
+/////////////////////////////////////////////
+// here is where we register our routes, this is how server.js knows to send the appropriate request to the appropriate route and send the correct response
+// app.use, when we register a route, needs two arguments
+// the first, is the base url endpoint, the second is the file to use
 
+app.use('/fruits', FruitRouter)
+app.use('/comments', commentRouter)
+app.use('/users', UserRouter)
+
+// this renders an error page, gets the error from a url request query
 app.get('/', (req, res) => {
     const { username, userId, loggedIn } = req.session
 	res.render('index.liquid', { loggedIn, username, userId })
 })
 
 app.get('/error', (req, res) => {
+	// get session variables
 	const error = req.query.error || 'This Page Does Not Exist'
     const { username, loggedIn, userId } = req.session
+
 	res.render('error.liquid', { error, username, loggedIn, userId })
 })
 
+// this is a catchall route, that will redirect to the error page for anything that doesn't satisfy a controller
 // if page is not found, send to error page
 app.all('*', (req, res) => {
 	res.redirect('/error')
@@ -48,3 +60,5 @@ app.all('*', (req, res) => {
 app.listen(process.env.PORT, () => {
     console.log(`listening on port ${process.env.PORT}`)
 })
+
+// END
